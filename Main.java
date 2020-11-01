03a90bb9a43e715718786847a2041f3620a24cc1
itatrajano <34098524+itatrajano@users.noreply.github.com>
2020-11-01 17:09:22 -0300

    Stage 2
    Correcting symbol level errors.

--- Main.java
+++ Main.java
@@ -1,17 +1,49 @@
 package correcter;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
-class RandomChar {
+class ErrorGenerator {
 
-    public static char getChar() {
+    //Creates randoms errors. One error at each group of three characters.
+    public static String getDamagedString(String originalString) {
+
+        char[] charsArray = originalString.toCharArray();
+
+        int inputSize = charsArray.length; //Number of characters
+        for (int i = 0; i < inputSize; i = i + 3) {
+            int numberOfCharsAtLeft = inputSize - 1 - i;
+            int randomBound;
+            switch (numberOfCharsAtLeft) { //To define the limits to be used at random.nextInt
+                case 0:
+                    randomBound = 1; //To draw 0, always
+                    break;
+                case 1:
+                    randomBound = 2; //To draw 0 or 1
+                    break;
+                default:
+                    randomBound = 3; //To draw 0, 1 or 2
+            }
+
+            Random random = new Random();
+            int zeroOneOrTwo = random.nextInt(randomBound); //Draw which element of the triple will be changed.
+            int targetIndex = i + zeroOneOrTwo;
+            char newChar;
+            do {
+                newChar = getRandomChar();
+            } while (newChar == charsArray[i]); //If the char to be changed is the same as the new char, try again.
+            charsArray[targetIndex] = newChar;
+        }
+        String damagedString = new String(charsArray);
+        return damagedString;
+    }
+
+    public static char getRandomChar() {
         Random random = new Random();
         char randomChar = 0;
-        boolean isCharSpace = randomChar == 32;
-        boolean isCharNumber = randomChar >= 48 && randomChar <= 57;
-        boolean isCharUppercase = randomChar >= 65 && randomChar <= 90;
-        boolean isCharLowercase = randomChar >= 97 && randomChar <= 122;
+        boolean isCharSpace = false;
+        boolean isCharNumber = false;
+        boolean isCharUppercase = false;
+        boolean isCharLowercase = false;
 
         while (!(isCharLowercase || isCharUppercase || isCharNumber || isCharSpace)) {
             randomChar = (char) random.nextInt(123);
@@ -24,42 +56,66 @@ class RandomChar {
     }
 }
 
+class EncoderDecoder {
+
+    public static String encoder(String inputString, int repetition) {
+        char[] inputCharArray = inputString.toCharArray();
+        char[] encodedCharArray = new char[inputCharArray.length * repetition];
+        for (int i = 0; i < inputCharArray.length; i++) { //For every char in the old array, add it "repetition" times to the new array
+            int startingIndex = i * repetition;
+            for (int j = 0; j < repetition; j++) {
+                encodedCharArray[startingIndex + j] = inputCharArray[i];
+            }
+        }
+        return String.copyValueOf(encodedCharArray);
+    }
+
+    public static String decoderAndRestorer(String inputString, int repetition) {
+        char[] inputCharArray = inputString.toCharArray();
+        char[] decodedCharArray = new char[inputCharArray.length / repetition];
+        for (int i = 0; i < inputCharArray.length; i = i + repetition) { //To each group of size "repetition"
+            //Decision process - The first index with more points is chosen.
+            int[] score = new int[repetition]; //Score of every char in "evaluatingGroup"
+            char[] evaluatingGroup = new char[repetition];
+            int evaluatingGroupIndex = 0;
+            for (int j = i; j < i + repetition; j++) { //Copy the group to be analyzed to a new array
+                evaluatingGroup[evaluatingGroupIndex] = inputCharArray[j];
+                evaluatingGroupIndex++;
+            }
+            for (char c: evaluatingGroup) { //Compare each char to every other char in ints evaluatingGroup
+                for (int j = 0; j < evaluatingGroup.length; j++) {
+                    if (c == evaluatingGroup[j]) {
+                        score[j]++;
+                    }
+                }
+            }
+
+            //Check the highest score
+            int chosenIndex = 0;
+            int higherScore = 0;
+            for (int j = 0; j < score.length; j++) {
+                if (score[j] > higherScore) {
+                    higherScore = score[j];
+                    chosenIndex = j;
+                }
+            }
+            decodedCharArray[i / repetition] = evaluatingGroup[chosenIndex];
+        }
+        return String.copyValueOf(decodedCharArray);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner =  new Scanner(System.in);
         String input = scanner.nextLine();
-        char[] charsArray = input.toCharArray();
-
-        //Creating random errors. One error at each group of three characters.
-        int inputSize = charsArray.length; //Number of characters
-
-
-        Random random = new Random();
-        for (int i = 0; i < inputSize; i = i + 3) {
-            int numberOfCharsAtLeft = inputSize - 1 - i;
-            int randomBound;
-            switch (numberOfCharsAtLeft) { //To define the limits to be used at random.nextInt
-                case 0:
-                    randomBound = 1; //To draw 0, always
-                    break;
-                case 1:
-                    randomBound = 2; //To draw 0, 1
-                    break;
-                default:
-                    randomBound = 3; //To draw 0, 1 or 2
-            }
-
-            int zeroOneOrTwo = random.nextInt(randomBound); //Draw which element of the triple will be changed.
-            int targetIndex = i + zeroOneOrTwo;
-            char newChar = 0;
-            do {
-                newChar = RandomChar.getChar();
-            } while (newChar == charsArray[i]);
-            charsArray[targetIndex] = newChar;
-        }
-
-        String damagedString = new String(charsArray);
+        System.out.println(input);
+        String encodedString = EncoderDecoder.encoder(input, 3);
+        System.out.println(encodedString);
+        String damagedString = ErrorGenerator.getDamagedString(encodedString);
         System.out.println(damagedString);
+        String restoredString = EncoderDecoder.decoderAndRestorer(damagedString, 3);
+        System.out.println(restoredString);
     }
 }
